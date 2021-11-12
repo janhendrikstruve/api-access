d850a04b14746f7fdb0ed06018173934376117eb
Jan <janhendrikstruve@gmail.com>
2021-11-12 17:07:08 +0100

    commented out not working features

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Stuff from './pages/Stuff/Stuff';
 import Dashboard from './pages/Dashboard/Dashboard';
-import AddForm from './pages/AddForm/AddForm';
+// import AddForm from './pages/AddForm/AddForm';
 
 function App(): JSX.Element {
   return (
@@ -12,7 +12,7 @@ function App(): JSX.Element {
         <Route path="/Stuff">
           <Route path=":chosenThing" element={<Stuff />} />
         </Route>
-        <Route path="/add" element={<AddForm />} />
+        {/* <Route path="/add" element={<AddForm />} /> */}
       </Routes>
     </BrowserRouter>
   );
--- src/app/pages/AddForm/AddForm.tsx
+++ src/app/pages/AddForm/AddForm.tsx
@@ -1,25 +1,25 @@
 import React from 'react';
 import Form from '../../components/Form/Form';
 
-export default function AddForm(): JSX.Element {
-  type addApiProps = {
-    event: React.FormEvent<HTMLFormElement>;
-  };
-  async function handleSubmit(event: addApiProps) {
-    event.preventDefault();
+// export default function AddForm(): JSX.Element {
+//   type addApiProps = {
+//     event: React.FormEvent<HTMLFormElement>;
+//   };
+//   async function handleSubmit(event: addApiProps) {
+//     event.preventDefault();
 
-    await fetch('...', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ name, description }),
-    });
+//     await fetch('...', {
+//       method: 'POST',
+//       headers: {
+//         'Content-Type': 'application/json',
+//       },
+//       body: JSON.stringify({ name, description }),
+//     });
 
-    return (
-      <>
-        <Form handleSubmit={handleSubmit}></Form>
-      </>
-    );
-  }
-}
+//     return (
+//       <>
+//        {/* <Form handleSubmit={handleSubmit}></Form> */}
+//       </>
+//     );
+//   }
+// }
