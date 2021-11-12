3b31521bf519b876a160c21e0bab7507943116a8
Jan <janhendrikstruve@gmail.com>
2021-11-12 17:04:43 +0100

    Add submit but not working yet

---
+++ src/app/pages/AddForm/AddForm.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Form from '../../components/Form/Form';
+
+export default function AddForm(): JSX.Element {
+  type addApiProps = {
+    event: React.FormEvent<HTMLFormElement>;
+  };
+  async function handleSubmit(event: addApiProps) {
+    event.preventDefault();
+
+    await fetch('...', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ name, description }),
+    });
+
+    return (
+      <>
+        <Form handleSubmit={handleSubmit}></Form>
+      </>
+    );
+  }
+}
